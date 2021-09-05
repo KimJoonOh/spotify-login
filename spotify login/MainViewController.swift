64667b7fcd47900792a3ad90a01fd6fae6de37c8
Joon <kjo11300@naver.com>
2021-09-05 19:15:02 +0900

    Add Email Login

--- spotify login/MainViewController.swift
+++ spotify login/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     @IBOutlet var welcomeLabel: UILabel!
@@ -18,10 +19,25 @@ class MainViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.isHidden = true
+//        navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+            환영합니다.
+            \(email)님
+            """
     }
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
         self.navigationController?.popToRootViewController(animated: true)
+        
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("ERROR: \(signOutError.localizedDescription)")
+        }
     }
 }
